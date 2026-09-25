b7407c8fddaa0c02a66b0f4ebbd0f6cd49420342
Isaac Junior <164945298+Leano-Jordan@users.noreply.github.com>
2026-09-25 21:04:56 +0000

    feat: add work edit routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ Route::get('/work', [WorkController::class, 'index'])->name('work.index');
 Route::get('/work/create', [WorkController::class, 'create'])->name('work.create');
 Route::post('/work', [WorkController::class, 'store'])->name('work.store');
 Route::get('/work/{event}', [WorkController::class, 'show'])->name('work.show');
+Route::get('/work/{event}/edit', [WorkController::class, 'edit'])->name('work.edit');
+Route::put('/work/{event}', [WorkController::class, 'update'])->name('work.update');
 
 Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
 Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
