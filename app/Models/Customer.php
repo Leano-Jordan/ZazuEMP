2b8a1a4652ed240c83cc8d8a5f72ec4d8f4c5f34
Isaac Junior <164945298+Leano-Jordan@users.noreply.github.com>
2026-09-25 20:31:55 +0000

    foundation: add Customer.php

---
+++ app/Models/Customer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'business_id',
+        'name',
+        'notes',
+    ];
+
+    public function business(): BelongsTo
+    {
+        return $this->belongsTo(Business::class);
+    }
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(CustomerContact::class);
+    }
+
+    public function events(): HasMany
+    {
+        return $this->hasMany(Event::class);
+    }
+
+    public function primaryContact(): HasOne
+    {
+        return $this->hasOne(CustomerContact::class)->where('is_primary', true);
+    }
+}
