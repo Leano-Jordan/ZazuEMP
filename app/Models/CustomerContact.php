1f77f392daefcb03589bf5c52bcb37e8afd3bba7
Isaac Junior <164945298+Leano-Jordan@users.noreply.github.com>
2026-09-25 22:02:31 +0000

    docs: clarify contact model properties for tooling

--- app/Models/CustomerContact.php
+++ app/Models/CustomerContact.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $customer_id
+ * @property string $name
+ * @property string|null $phone
+ * @property string|null $email
+ * @property string|null $label
+ * @property bool $is_primary
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read Customer $customer
+ */
 class CustomerContact extends Model
 {
     use HasFactory;
