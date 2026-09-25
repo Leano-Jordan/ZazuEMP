6ec0243705e074ef403f2a7e29a3d1cc808561b2
Isaac Junior <164945298+Leano-Jordan@users.noreply.github.com>
2026-09-25 20:56:08 +0000

    feat: wire Zazu work and customer routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\WorkController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => redirect()->route('work.index'));
+
+Route::get('/work', [WorkController::class, 'index'])->name('work.index');
+Route::get('/work/create', [WorkController::class, 'create'])->name('work.create');
+Route::post('/work', [WorkController::class, 'store'])->name('work.store');
+Route::get('/work/{event}', [WorkController::class, 'show'])->name('work.show');
+
+Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
+Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
+Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
