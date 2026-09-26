8b500b24b69d27ba206bf76a29adb98769434f84
Isaac Junior <164945298+Leano-Jordan@users.noreply.github.com>
2026-09-26 11:35:35 +0000

    arch: scope calendar to active business

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Support\CurrentBusiness;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -11,11 +12,13 @@ class CalendarController extends Controller
 {
     public function __invoke(Request $request): View
     {
+        $businessId = app(CurrentBusiness::class)->id($request->user());
         $month = $this->resolveMonth($request->string('month')->toString());
         $gridStart = $month->copy()->startOfMonth()->startOfWeek(Carbon::MONDAY);
         $gridEnd = $month->copy()->endOfMonth()->endOfWeek(Carbon::SUNDAY);
 
         $events = Event::query()
+            ->where('business_id', $businessId)
             ->with('customer')
             ->whereBetween('event_date', [$gridStart, $gridEnd])
             ->orderBy('event_date')
