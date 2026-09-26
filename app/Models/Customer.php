0213d9d873b569104cee723083752139d1882a37
Isaac Junior <164945298+Leano-Jordan@users.noreply.github.com>
2026-09-26 11:40:38 +0000

    arch: apply business ownership guard to model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\BelongsToBusiness;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Customer extends Model
 {
-    use HasFactory;
+    use BelongsToBusiness, HasFactory;
 
     protected $fillable = [
         'business_id',
