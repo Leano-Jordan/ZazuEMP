d0e42d737e1d7754e3e22e46cac91e557d3b3ba3
Isaac Junior <164945298+Leano-Jordan@users.noreply.github.com>
2026-09-26 01:41:25 +0000

    test: align root smoke test with dashboard redirect

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertRedirect(route('work.index'));
+        $response->assertRedirect(route('dashboard'));
     }
 }
