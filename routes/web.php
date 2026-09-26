e2684ccec899120a1e0e3ec477ba9cb9e834cc1d
Isaac Junior <164945298+Leano-Jordan@users.noreply.github.com>
2026-09-26 01:38:37 +0000

    fix: register customer and work lifecycle routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BusinessCapabilityController;
+use App\Http\Controllers\CustomerContactController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\RequirementController;
 use App\Http\Controllers\QuoteController;
@@ -29,6 +30,7 @@ Route::post('/work', [WorkController::class, 'store'])->name('work.store');
 Route::get('/work/{event}', [WorkController::class, 'show'])->name('work.show');
 Route::get('/work/{event}/edit', [WorkController::class, 'edit'])->name('work.edit');
 Route::put('/work/{event}', [WorkController::class, 'update'])->name('work.update');
+Route::delete('/work/{event}', [WorkController::class, 'destroy'])->name('work.destroy');
 
 Route::get('/work/{event}/travel', [TravelCostController::class, 'index'])->name('work.travel.index');
 Route::get('/work/{event}/travel/create', [TravelCostController::class, 'create'])->name('work.travel.create');
@@ -46,6 +48,13 @@ Route::get('/customers', [CustomerController::class, 'index'])->name('customers.
 Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
 Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
 Route::get('/customers/{customer}', [CustomerController::class, 'show'])->name('customers.show');
+Route::get('/customers/{customer}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
+Route::put('/customers/{customer}', [CustomerController::class, 'update'])->name('customers.update');
+Route::get('/customers/{customer}/contacts/create', [CustomerContactController::class, 'create'])->name('customers.contacts.create');
+Route::post('/customers/{customer}/contacts', [CustomerContactController::class, 'store'])->name('customers.contacts.store');
+Route::get('/customers/{customer}/contacts/{contact}/edit', [CustomerContactController::class, 'edit'])->name('customers.contacts.edit');
+Route::put('/customers/{customer}/contacts/{contact}', [CustomerContactController::class, 'update'])->name('customers.contacts.update');
+Route::delete('/customers/{customer}/contacts/{contact}', [CustomerContactController::class, 'destroy'])->name('customers.contacts.destroy');
 
 Route::get('/capabilities', [BusinessCapabilityController::class, 'index'])->name('capabilities.index');
 Route::get('/capabilities/create', [BusinessCapabilityController::class, 'create'])->name('capabilities.create');
