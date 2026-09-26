4d72aca8c8a1fcff229d19cc6daf76647c2c3453
Isaac Junior <164945298+Leano-Jordan@users.noreply.github.com>
2026-09-26 00:18:01 +0000

    Connect Zazu navigation and workflow pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BusinessCapabilityController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\RequirementController;
 use App\Http\Controllers\WorkController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,14 @@ Route::get('/work/{event}', [WorkController::class, 'show'])->name('work.show');
 Route::get('/work/{event}/edit', [WorkController::class, 'edit'])->name('work.edit');
 Route::put('/work/{event}', [WorkController::class, 'update'])->name('work.update');
 
+Route::get('/work/{event}/requirements', [RequirementController::class, 'index'])->name('work.requirements.index');
+Route::get('/work/{event}/requirements/create', [RequirementController::class, 'create'])->name('work.requirements.create');
+Route::post('/work/{event}/requirements', [RequirementController::class, 'store'])->name('work.requirements.store');
+
 Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
 Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
 Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
+Route::get('/customers/{customer}', [CustomerController::class, 'show'])->name('customers.show');
 
 Route::get('/capabilities', [BusinessCapabilityController::class, 'index'])->name('capabilities.index');
 Route::get('/capabilities/create', [BusinessCapabilityController::class, 'create'])->name('capabilities.create');
